06daee25e08a9175280f800b77debc4cf0adafca
Éverton Toffanetto <evertondgn@hotmail.com>
2024-06-15 16:06:20 -0300

    feat: initial commit

---
+++ scenes/levels/level.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+var laser_scene: PackedScene = preload('res://scenes/projectiles/laser.tscn')
+var grenade_scene: PackedScene = preload('res://scenes/projectiles/grenade.tscn')
+
+
+func _on_gate_player_entered_gate(body):
+	print('player has entered gate')
+	print(body)
+
+
+func _on_gate_player_exited_gate(body):
+	print('player has exited gate')
+	print(body)
+
+
+func _on_player_laser_fired(pos, direction):
+	var laser: Area2D = laser_scene.instantiate()
+	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
+	laser.direction = direction
+	$Projectiles.add_child(laser)
+
+
+func _on_player_granade_fired(pos, direction):
+	var grenade: RigidBody2D = grenade_scene.instantiate()
+	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
+	$Projectiles.add_child(grenade)
---
+++ scenes/player/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+
+signal laser_fired(pos, direction)
+signal granade_fired(pos, direction)
+
+
+var can_laser: bool = true
+var can_granade: bool = true
+
+
+func _process(_delta):
+	var direction = Input.get_vector('Left', 'Right', 'Up', 'Down')
+	velocity = direction * 500
+	move_and_slide()
+
+	look_at(get_global_mouse_position())
+
+	var player_direction = (get_global_mouse_position() - position).normalized()
+
+	if Input.is_action_pressed('Primary Action') and can_laser:
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		can_laser = false
+		$LaserReloadTimer.start()
+		laser_fired.emit(selected_laser.global_position, player_direction)
+
+	if Input.is_action_pressed('Secondary Action') and can_granade:
+		can_granade = false
+		$GrenadeReloadTimer.start()
+		var pos = $LaserStartPositions.get_children()[0].global_position
+		granade_fired.emit(pos, player_direction)
+
+
+func _on_laser_reload_timer_timeout():
+	can_laser = true
+
+
+func _on_grenade_reload_timer_timeout():
+	can_granade = true
